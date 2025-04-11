6de22031f987a915cf3c249e036166f52b9c8c54
joanzafdev <joanzafdev@gmail.com>
2025-04-11 13:35:50 +0200

    typescript react  is functional

---
+++ front/src/index.tsx
@@ -0,0 +1,12 @@
+import React, { useEffect } from "react";
+import Dashboard from "./Dashboard";
+
+function index(){
+    return(
+        <>
+            <Dashboard/>
+        </>
+    )
+}
+
+export default index;
